e0b699e3dc447bf7500534f8336013ea5cc64cef
Bernardo Gonzalez <60318571+bernigonzalez@users.noreply.github.com>
2022-04-19 19:44:28 -0300

    fix bug dploy

--- src/config/Fire.js
+++ src/config/Fire.js
@@ -3,14 +3,15 @@ import { initializeApp } from "firebase/app";
 import {API_KEY, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId} from "./utils"
 
 const config = {
+
   apiKey: API_KEY,
-  authDomain: authDomain,
-  databaseURL: databaseURL,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
-  measurementId: measurementId  
+  authDomain: "expense-tracker-d7b6d.firebaseapp.com",
+  databaseURL: "https://expense-tracker-d7b6d-default-rtdb.firebaseio.com",
+  projectId: "expense-tracker-d7b6d",
+  storageBucket: "expense-tracker-d7b6d.appspot.com",
+  messagingSenderId: "377290870016",
+  appId: "1:377290870016:web:5bc038306450e72b26f7e5",
+  measurementId: "G-13TJDC6C4D"  
 }
 
 const fire = firebase.initializeApp(config)
